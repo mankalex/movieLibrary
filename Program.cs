be77de0ccf2033c88d6932fe1637ac38ebdd50ca
alexm <amanka@my.wctc.edu>
2023-02-20 13:57:43 -0600

    [1.06] Create Read File

--- Program.cs
+++ Program.cs
@@ -8,11 +8,37 @@ StreamReader links = new StreamReader("links.csv");
 StreamReader ratings = new StreamReader("ratings.csv");
 StreamReader tags = new StreamReader("tags.csv");
 //prompt for input
-Console.WriteLine("Enter what movieId you wish to view.");
+Console.WriteLine("Enter 1 to add data.");
+Console.WriteLine("Enter 2 to read data.");
+Console.WriteLine("Enter anything else to quit.");
 // input response
 string? resp = Console.ReadLine();
 
-if(String.IsNullOrEmpty(resp)) {
-    logger.Error("Please enter a valid ID");
+if(resp == "1")
+{
+
+}
+else if(resp == "2")
+{
+    //reads the entire file
+    while(!movies.EndOfStream)
+    {
+        Console.WriteLine(movies.ReadLine());
+    }
+    //reads entire file
+    while(!links.EndOfStream)
+    {
+        Console.WriteLine(links.ReadLine());
+    }
+    //reads entire file
+    while(!ratings.EndOfStream)
+    {
+        Console.WriteLine(ratings.ReadLine());
+    }
+    //reads entire file
+    while(!tags.EndOfStream)
+    {
+        Console.WriteLine(tags.ReadLine());
+    }
 }
 
