4f56e93f7020bf1a7fc28e886715c8cc447e53a7
alexm <amanka@my.wctc.edu>
2023-02-20 13:19:21 -0600

    [1.05] Add Prompt for movie ID

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+
+StreamReader movies = new StreamReader("movies.csv");
+StreamReader links = new StreamReader("links.csv");
+StreamReader ratings = new StreamReader("ratings.csv");
+StreamReader tags = new StreamReader("tags.csv");
+//prompt for input
+Console.WriteLine("Enter what movieId you wish to view.");
+// input response
+string? resp = Console.ReadLine();
+
+if(String.IsNullOrEmpty(resp)) {
+    logger.Error("Please enter a valid ID");
+}
+
